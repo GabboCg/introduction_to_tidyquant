acd0dff390e339a01c1153690333299f865d1336
Gabriel Cabrera <gcabrerag@fen.uchile.cl>
2018-08-15 23:55:36 -0300

    Introduction to tidyquant

---
+++ class-27-08-2018.R
@@ -0,0 +1,10 @@
+#'---
+#'author: Gabriel Cabrera G.
+#'title: Introduction to tidyquant
+#'subtitle: Bringing financial analysis to the tidyverse
+#'date: 27/08/2018
+#'---
+
+if(!require("pacman")) install.packages("pacman")
+p_load("tidyverse","tidyquant")
+
